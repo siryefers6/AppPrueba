3c8350990e69af42da077f9ea26fdf87f90dd08e
yeferson <siryefers@gmail.com>
2025-03-24 05:37:05 -0300

    docs: Agregar comentarios explicativos

--- app/src/main/java/com/example/appprueba/MainActivity.kt
+++ app/src/main/java/com/example/appprueba/MainActivity.kt
@@ -20,32 +20,58 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 
-
+// MainActivity es la clase principal de nuestra aplicación
 class MainActivity : ComponentActivity() {
+
+    // onCreate es el método que se ejecuta al iniciar la aplicación
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // super.onCreate(savedInstanceState) es el método que se ejecuta al iniciar la aplicación
         super.onCreate(savedInstanceState)
+
+        // enableEdgeToEdge es el método que nos permite habilitar el modo de pantalla completa
         enableEdgeToEdge()
+
+        // setContent es el método que nos permite definir el contenido de la aplicación
         setContent {
+
+            // Modifier es un objeto que nos permite definir las propiedades de un componente
             MiPrimerComposable()
         }
     }
 }
 
+
+// Preview es una vista previa de nuestro composable
 @Composable
+
+// MiPrimerComposable es un composable que nos permite mostrar un texto
 fun MiPrimerComposable() {
 
+    // Scaffold es un contenedor que nos permite tener un AppBar y un Body
     Column(modifier = Modifier.fillMaxSize().padding(top=35.dp)) {
 
+        // Row es un contenedor que nos permite tener elementos en una fila
         Row {
 
+            // Text es un componente que nos permite mostrar texto
             Text(text="Texto fila 1")
+
+            // Spacer es un componente que nos permite agregar espacio
             Spacer(modifier = Modifier.width(35.dp))
+
+            // Text es un componente que nos permite mostrar texto
             Text(text="Texto fila 1B")
 
         }
 
+        // Spacer es un componente que nos permite agregar espacio
         Spacer(modifier = Modifier.height(35.dp))
+
+        // Text es un componente que nos permite mostrar texto
         Text(text="Hola alumnos de kotlin")
+
+        // Spacer es un componente que nos permite agregar espacio
         Text(text="Este es nuestro primer compostable")
 
     }
